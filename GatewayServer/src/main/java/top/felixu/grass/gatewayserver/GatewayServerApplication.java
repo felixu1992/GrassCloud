fab2d06cd78afd0e801c10c1db8f24911ed28b2c
felixu <xufei_0320@163.com>
2019-04-11 23:21:35 +0800

    Add Gateway And Oauth Server Module
    Add Module

---
+++ GatewayServer/src/main/java/top/felixu/grass/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.grass.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
---
+++ OauthServer/src/main/java/top/felixu/grass/oauthserver/OauthServerApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.grass.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthServerApplication.class, args);
+    }
+
+}
