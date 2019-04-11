667d311348e1fc67dbc2f3d43d845c5f4e73daf5
felixu <xufei_0320@163.com>
2019-04-11 18:15:04 +0800

    Add ConfigServer Module
    Add module

---
+++ ConfigServer/src/main/java/top/felixu/grass/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.grass.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
