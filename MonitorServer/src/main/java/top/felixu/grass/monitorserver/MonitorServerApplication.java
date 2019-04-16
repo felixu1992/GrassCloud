7b6a8754731da51ebc342d55d40335728878b01c
felixu <xufei_0320@163.com>
2019-04-16 14:48:18 +0800

    Add Monitor Server Module
    add module

---
+++ MonitorServer/src/main/java/top/felixu/grass/monitorserver/MonitorServerApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.grass.monitorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorServerApplication.class, args);
+    }
+
+}
