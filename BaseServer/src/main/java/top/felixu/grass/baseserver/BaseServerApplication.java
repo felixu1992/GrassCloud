aee7ad971ada0b36210083889841ec3c43778934
felixu <xufei_0320@163.com>
2019-04-16 15:44:25 +0800

    Add Base Server Module
    add module

---
+++ BaseServer/src/main/java/top/felixu/grass/baseserver/BaseServerApplication.java
@@ -0,0 +1,13 @@
+package top.felixu.grass.baseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseServerApplication.class, args);
+	}
+
+}
---
+++ BaseServer/src/test/java/top/felixu/grass/baseserver/BaseServerApplicationTests.java
@@ -0,0 +1,16 @@
+package top.felixu.grass.baseserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
