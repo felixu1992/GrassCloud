40620fe7ac67f793e47e8018ca62f690593f19a1
felixu <xufei_0320@163.com>
2019-07-25 11:44:21 +0800

    update

--- common/common-core/src/main/java/top/felixu/grass/common/core/exception/GrassExceptionHandler.java
+++ common/common-core/src/main/java/top/felixu/grass/common/core/exception/GrassExceptionHandler.java
@@ -3,9 +3,8 @@ package top.felixu.grass.common.core.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import top.felixu.grass.common.core.dto.BaseResp;
 
 import javax.servlet.http.HttpServletResponse;
@@ -18,8 +17,7 @@ import java.nio.file.AccessDeniedException;
  * @date 2019.06.11
  */
 @Slf4j
-@ResponseBody
-@ControllerAdvice
+@RestControllerAdvice
 public class GrassExceptionHandler {
 
     @ExceptionHandler
