148c91e82c249267911058e395ad99d3087ec760
felixu <xufei_0320@163.com>
2019-08-20 17:46:14 +0800

    login

---
+++ base-server/src/main/java/top/felixu/grass/baseserver/constants/CacheConstants.java
@@ -0,0 +1,18 @@
+package top.felixu.grass.baseserver.constants;
+
+/**
+ * 缓存常量池
+ *
+ * @author felixu
+ * @date 2019.08.20
+ */
+public interface CacheConstants {
+
+    interface Value {
+        String USER = "user";
+    }
+
+    interface Key {
+        String PREFIX = "grass_";
+    }
+}
--- base-server/src/main/java/top/felixu/grass/baseserver/service/UserInfoService.java
+++ base-server/src/main/java/top/felixu/grass/baseserver/service/UserInfoService.java
@@ -7,16 +7,21 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 import top.felixu.grass.baseserver.entity.UserInfo;
 import top.felixu.grass.baseserver.mapper.UserInfoMapper;
 import top.felixu.grass.baseserver.properties.AuthProperties;
 import top.felixu.grass.baseserver.remote.OauthServerClient;
 import top.felixu.grass.common.core.dto.base.LoginDTO;
 import top.felixu.grass.common.core.dto.oauth.JwtDTO;
+import top.felixu.grass.common.core.exception.ErrorCode;
+import top.felixu.grass.common.core.exception.GrassException;
 import top.felixu.grass.common.core.form.base.AddLoginRecordForm;
 import top.felixu.grass.common.core.form.base.LoginForm;
 import top.felixu.grass.common.core.utils.HttpUtils;
+import top.felixu.grass.common.core.utils.MD5Utils;
 
 import java.time.LocalDateTime;
 
@@ -43,9 +48,16 @@ public class UserInfoService extends ServiceImpl<UserInfoMapper, UserInfo> imple
         // 错误锁定逻辑
         // TODO 密码校验规则，可以用全局缓存来做，减少一次IO
         UserInfo user = findByLogin(form.getUsername());
+        if (!MD5Utils.matches(form.getPassword(), user.getPassword())) {
+            throw new GrassException(ErrorCode.PASSWORD_ERROR);
+        }
         JwtDTO jwt = oauthServerClient.getToken(authProperties.getValue(),
                 authProperties.getType(), user.getEmployeeNum(), form.getPassword());
         log.info("Login Success: user={}, jwt={}", user, jwt);
+        if (ObjectUtils.isEmpty(jwt)
+                || ObjectUtils.isEmpty(jwt.getAccessToken())) {
+            throw new GrassException(ErrorCode.LOGIN_ERROR);
+        }
         LoginDTO loginUser = new LoginDTO();
         loginUser.setUserId(user.getId());
         loginUser.setToken(jwt.getAccessToken());
@@ -65,6 +77,7 @@ public class UserInfoService extends ServiceImpl<UserInfoMapper, UserInfo> imple
         return loginUser;
     }
 
+    @Cacheable(value = "user", key = "'grass_' + #result.getId()", unless = "#result == null")
     public UserInfo findByLogin(String loginParam) {
         LambdaQueryWrapper<UserInfo> wrapper = new QueryWrapper<UserInfo>().lambda()
                 .eq(UserInfo::getLoginName, loginParam).or()
